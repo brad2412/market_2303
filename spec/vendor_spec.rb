7d140b64967f1a33122db12649e2adacc325acf5
Bradley Milton <bmilton2412@gmail.com>
2023-04-17 11:01:50 -0700

    add tests to the new market class for add vendor, vendor names, vendors that sell methods.  was refining potential revenue method and test as well

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,5 +1,6 @@
 require './lib/item'
 require './lib/vendor'
+require './lib/market'
 
 RSpec.describe Vendor do 
   describe 'Iteration 1' do
@@ -28,4 +29,22 @@ RSpec.describe Vendor do
       vendor.inventory
     end
   end
+
+  # describe '#potential_revenue method' do
+  #   it 'can show potential revenue of vendor' do
+  #     market = Market.new("South Pearl Street Farmers Market")
+  #     vendor1 = Vendor.new("Rocky Mountain Fresh")
+  #     vendor2 = Vendor.new("Ba-Nom-a-Nom")
+  #     vendor3 = Vendor.new("Palisade Peach Shack")
+  #     item1 = Item.new({name: 'Peach', price: "$0.75"})
+  #     item2 = Item.new({name: 'Tomato', price: "$0.50"})
+  #     item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+  #     item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+
+  #     expect(vendor1.potential_revenue).to eq(29.75)
+  #     expect(vendor2.potential_revenue).to eq(345.00)
+  #     expect(vendor3.potential_revenue).to eq(48.75)
+  #   end
+  # end
 end
